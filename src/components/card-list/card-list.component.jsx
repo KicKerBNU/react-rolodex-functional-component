4957b72f8b021f078a32727906c3516ec85d28a8
Everton Buzzi <evertonbuzzi@gmail.com>
2023-03-13 08:26:06 +0000

    #Rolodex-functional-component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,20 +1,11 @@
-import { Component } from "react";
 import './card-list.styles.css';
 import Card from "./card.component";
 
-class CardList extends Component {
-    render() {
-        const { monsters } = this.props;
-        
-        return  (
-            <div className="card-list">
-                {   monsters.map((monster) => {
-                        return ( <Card monster={monster} key={monster.id} /> )
-                    })
-                }
-            </div>
-        );
-    }
-}
-
+const CardList = ({monsters}) => (
+    <div className="card-list">
+        {monsters.map((monster) => {
+            return ( <Card monster={monster} key={monster.id} /> )
+        })}
+    </div>
+    );
 export default CardList;
